6e8b725056cebdb22b1d8df757d7b340aafa5f6e
iica <61383861+iicafiaxus@users.noreply.github.com>
2020-10-04 11:21:42 +0900

    コンパイル時引数にした

--- segtree.d
+++ segtree.d
@@ -1,37 +1,62 @@
 /*
 セグメント木 区分木 segtree segment tree
-1点に書き込み、範囲で読み出す。
+
+1点に書き込み、範囲で読み出す。(単位元の存在を仮定)
 たとえば [l, r) の最大値を求めるなど。
 
-引数
-a, b　添字の範囲。 この意味はa から b - 1 までなので注意。
-merge　左の子と右の子から自分の値をきめる関数（たとえばmax）
-neutral　すべての x に対して merge(x, z) = x をみたすz（たとえば-infty）
+使い方
+
+準備
+	auto seg = new SegTree!(
+		long,
+			// 値の型
+		delegate (long x, long y) => x + y,
+			// 左の子と右の子から自分の値をきめる関数（たとえばmax）
+		0
+			// すべての x に対して merge(x, z) = x をみたすz（たとえば-infty）
+			// 配列はこの値で初期化される
+	)(a, b);
+		// 添字の範囲。 この意味は a 以上 b - 1 以下ということ
+    
+書き込み
+	seg.setValue(i, x);
+		// 添字 i の位置に値 x をセットする
+
+読み出し
+	long ans = seg.getValue(a, b)
+		// a 以上 b - 1 以下の部分についての計算結果
+
+テストコード
+	AOJ DSL_2_A
+	https://paiza.io/projects/e/52K1iUD5pHv0OES3Sle14Q
 
 */
-class SegTree(T){
-	T value;
-	T neutral;
+class SegTree(
+	T,
+	T delegate(T, T) merge,
+	T neutral
+){
+	int a, b; // a <= x < b を担当する
 	SegTree left, right; // 左の子(a <= x < c)、右の子(c <= x < b)
+	T value;
 	bool hasValue;
 	bool isTerminal;
-	int a, b; // a <= x < b を担当する
-	T delegate(T, T) merge;
-	this(int a, int b, T delegate(T, T) merge, T neutral = T.init){
+	this(int a, int b){
 		this.a = a, this.b = b;
-		this.merge = merge;
-		this.neutral = neutral;
 		if(b - a == 1){
+    		this.value = neutral;
+    		this.hasValue = 1;
 			this.isTerminal = 1;
 		}
 		else{
 			int c = (a + b) / 2;
-			this.left = new SegTree(a, c, merge, neutral);
-			this.right = new SegTree(c, b, merge, neutral);
+			this.left = new SegTree(a, c);
+			this.right = new SegTree(c, b);
 		}
 	}
 	
 	// [a, b) における値を読む ※[a, b) はこのノードの担当区間とかぶっていなくてもよい
+	T getValue(){ return getValue(a, b); }
 	T getValue(int a, int b){
 		if(b <= this.a || this.b <= a) return neutral;
 		if(a <= this.a && this.b <= b){
@@ -73,36 +98,4 @@ class SegTree(T){
 	override string toString(){
 		return this.array.to!string;
 	}
-}
-
-// テストコード
-// 1行目で N を入力すると長さNのセグメント木を作成して適当に埋める
-// 2行目以降は l r の形のクエリ（-1 -1 を入力すると終了）
-import std.stdio, std.string, std.conv, std.array, std.math, std.algorithm;
-import std.datetime.stopwatch, std.random;
-void main(){
-	int n = readln.chomp.to!int;
-	auto seg = new SegTree!long(0, n, (long x, long y) => max(x, y));
-	
-	StopWatch sw;
-	sw.start;
-	foreach(i; 0 ..n) seg.setValue(i, (i + 1) * (i + 1) * (i + 1) % n);
-	sw.peek.writeln;
-	
-	long[] ans;
-	foreach(_; 0 .. n){
-		/*
-		int[] xs = readln.chomp.split.map!(to!int).array;
-		int l = xs[0] - 1, r = xs[1];
-		*/
-		int i = uniform(0, n), v = uniform(0, n);
-		seg.setValue(i, v);
-		int l = uniform(0, n), r = uniform(0, n);
-		if(l > r) continue;
-		else ans ~= seg.getValue(l, r);
-	}
-	sw.peek.writeln;
-	
-	ans.sum.writeln;
-	
 }
