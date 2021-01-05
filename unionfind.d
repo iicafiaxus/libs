848c38d2f69287f2c0bb439b5ec24451ac43343f
iica <61383861+iicafiaxus@users.noreply.github.com>
2021-01-06 01:03:31 +0900

    二部グラフ判定機能つきを追加

--- unionfind.d
+++ unionfind.d
@@ -1,3 +1,10 @@
+/*
+　素集合データ構造
+　　・基本的なもの
+　　・二部グラフ判定機能つき
+*/
+
+// 基本
 import std.conv;
 class UnionFind{
     int[] rootOf;
@@ -20,6 +27,47 @@ class UnionFind{
         return kidsOf[rootOf[a]].length.to!int;
     }
 }
+
+// 二部グラフ判定機能つき
+import std.conv;
+class UnionFind{
+    int[] rootOf;
+    int[][] kidsOf;
+	bool[] isNotBipart;
+	int[] color;
+    this(int n){
+        rootOf = new int[](n);
+        kidsOf = new int[][](n);
+        foreach(i; 0 .. n) rootOf[i] = i, kidsOf[i] = [i];
+		isNotBipart = new bool[](n);
+		color = new int[](n);
+    }
+    void unite(int a, int b){
+        int ra = rootOf[a], rb = rootOf[b];
+        if(ra == rb){
+			if(color[a] == color[b]) isNotBipart[ra] = 1;
+		}
+        else if(kidsOf[ra].length < kidsOf[rb].length) unite(b, a);
+        else{
+			bool f = (color[a] == color[b]);
+			foreach(k; kidsOf[rb]){
+				rootOf[k] = ra, kidsOf[ra] ~= k;
+				if(f) color[k] = 1 - color[k];
+			}
+			isNotBipart[ra] |= isNotBipart[rb];
+		}
+    }
+    int find(int a){
+        return rootOf[a];
+    }
+    int getSize(int a){
+        return kidsOf[rootOf[a]].length.to!int;
+    }
+	bool isBipart(int a){
+		return ! isNotBipart[rootOf[a]];
+	}
+}
+
 
 // ---------- 以下テスト ---------- //
 // https://judge.yosupo.jp/problem/unionfind
