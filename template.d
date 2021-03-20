2ea065ae99b3a7bd240c22a2d2b6fa21d5027fee
iica <61383861+iicafiaxus@users.noreply.github.com>
2021-03-21 01:50:24 +0900

    unsplitが区切り文字を引数に取れるように

--- template.d
+++ template.d
@@ -5,7 +5,7 @@ void main(string[] args){ if(args.canFind("-debug")) DEBUG = 1;
 if(args.canFind("-gen")) gen; else if(args.canFind("-jury")) jury; else solve; }
 void print(){ writeln(""); } void print(T)(T t){ writeln(t); }
 void print(T, A ...)(T t, A a){ std.stdio.write(t, " "), print(a); }
-string unsplit(T)(T xs){ return xs.array.to!(string[]).join(" "); }
+string unsplit(T)(T xs, string d=" "){ return xs.array.to!(string[]).join(d); }
 string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
 T scan(T)(){ return scan.to!T; } T[] scan(T)(int n){ return n.iota.map!(i => scan!T()).array; }
 T mini(T)(ref T x, T y){ if(x > y) x = y; return x; } T maxi(T)(ref T x, T y){ if(x < y) x = y; return x; }
