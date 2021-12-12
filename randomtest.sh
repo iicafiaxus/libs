0250d8600406b4b5284a6d9b65101a38a523b83c
iica <61383861+iicafiaxus@users.noreply.github.com>
2021-12-13 01:29:15 +0900

    スペシャルジャッジに対応

--- randomtest.sh
+++ randomtest.sh
@@ -8,13 +8,16 @@ fi
 i=0
 while true; do
 	i=`expr $i + 1`
-	$1 -gen > $1_rand.txt
-	yourans=$($1 < $1_rand.txt)
-	juryans=$($1 -jury < $1_rand.txt)
+	$1 -gen > $1_rand_in.txt
+	$1 < $1_rand_in.txt > $1_rand_out.txt
+	yourans=$(cat $1_rand_out.txt)
+	cat $1_rand_in.txt > $1_rand_inout.txt
+	cat $1_rand_out.txt >> $1_rand_inout.txt
+	juryans=$($1 -jury < $1_rand_inout.txt)
 	if [ "$yourans" != "$juryans" ]; then
 		echo "Test #$i: Wrong answer"
 		echo "Input:"
-		cat $1_rand.txt
+		cat $1_rand_in.txt
 		echo "Your answer:"
 		echo $yourans
 		echo "Jury's answer:"
